342ec088a82d6f90221f9b8b069fb6577a299e0f
tarekeesa <tarekeesa7@gmail.com>
2024-04-28 05:10:09 -0400

    Hello World

--- ecom_platform/ecommerce/settings.py
+++ ecom_platform/ecommerce/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,7 @@ SECRET_KEY = "django-insecure-%mi@0md3=3b@x(fis6b0pb+uzq3(x12(%xbz(dq^5brd)nq=)g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -54,7 +55,7 @@ ROOT_URLCONF = "ecommerce.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": ['templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
--- ecom_platform/ecommerce/urls.py
+++ ecom_platform/ecommerce/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from ecommerce.views import home_page
+
 urlpatterns = [
+    path('', home_page, name='home'),
     path("admin/", admin.site.urls),
 ]
